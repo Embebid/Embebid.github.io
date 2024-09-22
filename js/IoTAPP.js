20f44369aca00bdecbd8ddddfee9ff8cc2fec837
Embebid <embebidosmj@gmail.com>
2024-09-22 00:04:20 -0600

    Update IoTAPP.js
    cocine

--- js/IoTAPP.js
+++ js/IoTAPP.js
@@ -11,41 +11,45 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-var IoTApp = document.getElementById('IoTApp')
-var dbRef = firebase.database()
-
+var IoTApp = document.getElementById('IoTApp');
+var dbRef = firebase.database();
 
 var Altitud = 0;
 var Temp = 0;
 var Presion = 0;
 
-
 // Lectura de la altitud
 let dbAltitud = dbRef.ref("ESP32IoTApp/Altitud/");
 dbAltitud.on('value', function(snapshot) {
-  console.log("El valor de la Altitud es", snapshot.val());
-  Altitud = snapshot.val();
-  document.getElementById("AltId").innerHTML = Altitud;
+  let altitudValue = snapshot.val();
+  if (altitudValue != null) {
+    Altitud = parseFloat(altitudValue).toFixed(2);  // Limitar a 2 decimales
+    document.getElementById("AltId").innerHTML = Altitud + " m";
+  } else {
+    console.log("No se recibió ningún valor para la altitud");
+  }
 });
 
-// Lectura de la presion
+// Lectura de la presión
 let dbPresion = dbRef.ref("ESP32IoTApp/Presion/");
 dbPresion.on('value', function(snapshot) {
-  console.log("El valor de la Presion es", snapshot.val());
-  Presion = snapshot.val();
-  document.getElementById("PresionId").innerHTML = Presion;
+  let presionValue = snapshot.val();
+  if (presionValue != null) {
+    Presion = parseFloat(presionValue).toFixed(2);  // Limitar a 2 decimales
+    document.getElementById("PresionId").innerHTML = Presion + " mmHg";
+  } else {
+    console.log("No se recibió ningún valor para la presión");
+  }
 });
 
 // Lectura de la temperatura
 let dbTemp = dbRef.ref("ESP32IoTApp/Temp/");
 dbTemp.on('value', function(snapshot) {
-  console.log("El valor de la Temperatura es", snapshot.val());
-  Temp = snapshot.val();
-  document.getElementById("TemperaturaId").innerHTML = Temp;
+  let tempValue = snapshot.val();
+  if (tempValue != null) {
+    Temp = parseFloat(tempValue).toFixed(2);  // Limitar a 2 decimales
+    document.getElementById("TemperaturaId").innerHTML = Temp + " °C";
+  } else {
+    console.log("No se recibió ningún valor para la temperatura");
+  }
 });
-
-
-
-
-
-
